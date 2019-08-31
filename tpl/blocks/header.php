c8c4c3a30f099cc511174913dd8d6d7122e210a7
bombin78 <bombin78@mail.ru>
2019-08-31 11:04:19 +0300

    Переменная $path

--- tpl/blocks/header.php
+++ tpl/blocks/header.php
@@ -1,7 +1,7 @@
 <header class="header">
 	<div class="header__block">
 		<div class="header__block-control control-panel">
-			<a class="control-panel__logo" href="o-kompanii"><b><?php echo $nameCompany; ?></b></a>
+			<a class="control-panel__logo" href="<?php echo $path; ?>"><b><?php echo $nameCompany; ?></b></a>
 		</div>
 	</div>
 </header>
