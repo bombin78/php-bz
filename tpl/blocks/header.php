1eee80eafcc6123274cfefafd6c00f2d44bc013f
bombin78 <bombin78@mail.ru>
2019-12-18 00:45:29 +0300

    Верска шапки

--- tpl/blocks/header.php
+++ tpl/blocks/header.php
@@ -1,7 +1,30 @@
 <header class="header">
-	<div class="header__block">
-		<div class="header__block-control control-panel">
-			<a class="control-panel__logo" href="<?php echo $path; ?>"><b><?php echo $nameCompany; ?></b></a>
+	<div class="header__links">
+		<ul class="header__list link-list">
+			<li class="link-list__item">
+				<a class="link-list__home" href="/"></a>
+			</li>
+			<li class="link-list__item">
+				<a class="link-list__mail" href="/"></a>
+			</li>
+			<li class="link-list__item">
+				<a class="link-list__sitemap" href="/"></a>
+			</li>
+		</ul>
+	</div>
+	<div class="header__contacts">
+		<a class="header__brend" href="/">
+			<img class="brend__img" src="./images/layout/logo.gif" alt="Бушевецкий завод">
+		</a>
+		<dl class="header__address dtl">
+			<dt class="dtl__l">Бушевецкий завод, г. Бологое:</dt>
+			<dd class="dtl__v">+7(920) 194-84-13</dd>
+		</dl>
+		<div class="header__email">
+			<a class="header__email-link" href="mailto:bushevec2009@yandex.ru">
+				bushevec2009@yandex.ru
+			</a>
 		</div>
 	</div>
+	<div class="header__info"></div>
 </header>
