49dd5e4949cbd664294abbd51f0fcc46ed4323aa
bombin78 <bombin78@mail.ru>
2020-02-02 15:08:29 +0300

    footer: правки, добавлен счетчик "LiveInternet"

--- tpl/blocks/footer.php
+++ tpl/blocks/footer.php
@@ -1,3 +1,21 @@
 <footer class="footer">
-	<div class="footer__inner"></div>
+	<div class="footer__counter">
+		<div class="footer__counter-li live-internet">
+			<!--LiveInternet counter-->
+			<script type="text/javascript"><!--
+			document.write("<a href='http://www.liveinternet.ru/click' "+
+			"target=_blank><img src='http://counter.yadro.ru/hit?t45.1;r"+
+			escape(document.referrer)+((typeof(screen)=="undefined")?"":
+			";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+			screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+			";"+Math.random()+
+			"' alt='' title='LiveInternet' "+
+			"border='0' width='31' height='31'><\/a>")
+			//--></script>
+			<a href="http://www.liveinternet.ru/click" target="_blank">
+				<img src="http://counter.yadro.ru/hit?t45.1;r;s1366*768*24;uhttp%3A//www.bushevec.ru/;0.7323596708074347" alt="" title="LiveInternet" border="0" width="31" height="31">
+			</a>
+			<!--/LiveInternet-->
+		</div>
+	</div>
 </footer>
