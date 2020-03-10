97d4e5cc2c2bab4b180ad50b764892ab0336f0c8
bombin78 <bombin78@mail.ru>
2020-03-11 02:00:57 +0300

    карта сайта

---
+++ tpl/pages/map.php
@@ -0,0 +1,121 @@
+<section class="page">
+
+	<h1 class="page__content-title content-title"><?php echo $title; ?></h1>
+
+	<ul class="map-level-0">
+		<li>
+			<a href="/recuperators/">Рекуператор тепла</a>
+			<ul class="map-level-1">
+
+				<li><a href="/recuperators/recuperator-tepla-zakaz/">Рекуператор тепла</a></li>
+
+				<li><a href="/recuperators/recuperator-industry-5/">Рекуператоры нового поколения</a></li>
+
+				<li><a href="/recuperators/news/">Новости</a></li>
+
+				<li><a href="/recuperators/form/">Опросный лист на теплообменники</a></li>
+
+			</ul>
+		</li>
+
+		<li><a href="/capital-remont/">Капитальный ремонт</a>
+			<ul class="map-level-1">
+
+				<li><a href="/capital-remont/vibro-immersers/">Вибропогружатели</a></li>
+
+				<li><a href="/capital-remont/drezin/">Дрезины, автомотрисы, мотовозы</a></li>
+
+				<li><a href="/capital-remont/cotlovanokopately-base/">Котлованокопатели на любой базе</a>					<ul class="map-level-2">
+
+				<li><a href="/capital-remont/cotlovanokopately-base/VK-base-DGKU/">ВК на базе дрезины ДГКУ</a></li>
+
+				<li><a href="/capital-remont/cotlovanokopately-base/VK3-rail/">ВК-3 на базе ЖД платформы</a></li>
+
+				<li><a href="/capital-remont/cotlovanokopately-base/MPT-VK-5/">МПТ-ВК-5 на базе мотовоза МПТ-6</a></li>
+
+				<li><a href="/capital-remont/cotlovanokopately-base/ADM-1.5B/">АДМ-1.5 Б исп. 6.1 на базе дрезины АДМ-1.3</a></li>
+
+				<li><a href="/capital-remont/cotlovanokopately-base/VK-4M-rail/">ВК-4М на базе  ЖД платформы</a></li>
+
+			</ul>
+		</li>
+
+		<li><a href="/spare-parts/">Запчасти к машинам ВК и АВФ</a></li>
+
+		<li>
+			<a href="/navesnoe-oborudovanie/">Навесное оборудование для АВФ и МС</a>
+			<ul class="map-level-1">
+
+				<li><a href="/navesnoe-oborudovanie/burovoe-oborudovanie/">ТТН-057 для бурения лидирующих скважин</a></li>
+
+				<li><a href="/navesnoe-oborudovanie/fundamend/">ТТН-059 по разрушению фундамента</a></li>
+
+				<li><a href="/navesnoe-oborudovanie/vint/">ТТН-064 для завинчивания опор</a></li>
+
+			</ul>
+		</li>
+
+		<li>
+			<a href="/manufacturing/">Изготовление новых машин</a>
+			<ul class="map-level-1">
+
+				<li><a href="/manufacturing/kotlovanokopateli/">Котлованокопатели</a>					<ul class="map-level-2">
+
+				<li><a href="/manufacturing/kotlovanokopateli/VK-3-rail/">ВК-3 на базе ЖД платформы</a></li>
+
+				<li><a href="/manufacturing/kotlovanokopateli/MPT-VK-5/">МПТ-ВК-5 на базе мотовоза МПТ-6</a></li>
+
+				<li><a href="/manufacturing/kotlovanokopateli/VK-4-rail/">ВК-4 на базе ЖД платформы</a></li>
+
+				<li><a href="/manufacturing/kotlovanokopateli/ADM-1.5B/">АДМ-1.5 Б исп. 6.1 на базе дрезины АДМ-1.3</a></li>
+
+			</ul>
+		</li>
+
+		<li><a href="/manufacturing/vibro-immersers/">Вибропогружатели – АВФ-1</a></li>
+
+		<li>
+			<a href="/buying-used-eqipment/">Приобретение б/у техники</a>
+
+			<ul class="map-level-1">
+
+				<li><a href="/buying-used-eqipment/fitting-platforms/">Фитинговые платформы</a></li>
+
+				<li><a href="/buying-used-eqipment/drezins/">Дрезины ДГКУ, АДМ, МПТ</a></li>
+
+				<li><a href="/buying-used-eqipment/kotlovanokopateli/">Котлованокопатели и вибропогружатели</a></li>
+
+			</ul>
+		</li>
+
+		<li>
+			<a href="/material/">Материалы</a>
+			<ul class="map-level-1">
+
+				<li><a href="/material/drezina/">Дрезина, ремонт дрезин</a></li>
+
+				<li><a href="/material/avtomatrisa/">Виды автоматрис и их назначение</a></li>
+
+				<li><a href="/material/motovoz/">Мотовоз, ремонт мотовоза</a></li>
+
+				<li><a href="/material/vibropogruzhatel/">Вибропогружатель</a>
+					<ul class="map-level-2">
+
+						<li><a href="/material/vibropogruzhatel/princip/">Принцип действия</a></li>
+
+						<li><a href="/material/vibropogruzhatel/tehn-haracter/">Технические характеристики</a></li>
+
+					</ul>
+				</li>
+			</ul>
+		</li>
+
+		<li><a href="/news/">Новости</a></li>
+
+		<li><a href="/contact/">Контактная информация</a></li>
+
+		<li><a href="/video/">Видеоролики</a></li>
+
+	</ul>
+
+</section>
