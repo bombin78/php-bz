b4f8c8dfa13ffff9bdb83be949f87a971393af60
bombin78 <bombin78@mail.ru>
2020-07-02 11:55:26 +0300

    Раздел "Рекуператоры": доработка дополнительных подразделов

--- tpl/pages/recuperators/articles.php
+++ tpl/pages/recuperators/articles.php
@@ -5,10 +5,33 @@
 	<div class="description">
 
 		<div class="description__row">
+
 			<a class="bnrc" href="http://rekuperator.ru/">
 				<img class="bnrc__img" src="/images/pages/recuperators/rekuperator.ru.jpg">
 			</a>
-		</div>
+
+          	<p class="description__text">
+      			Деятельность Бушевецкого завода по производству теплообменников отражена на на сайте "
+      			<a href="http://rekuperator.ru/" title="Пластинчатые рекуператоры Бушевец - Производство пластинчатых теплообменников. Теплообменники нового поколения" target="_blank">
+              		Rekuperator.ru - пластинчатые теплообменники нового поколения
+              	</a>"
+      		</p>
+
+            <p class="description__text"><strong>Дополнительные материалы по теме "Рекуператоры"</strong></p>
+
+            <dl class="additional-material">
+                <dt class="additional-material__label">
+                    Уважаемые посетители сайта, для того, чтобы у вас сложилось представление о разнообразных технологиях и
+                    сферах применения рекуператоров, предлагаем ознакомиться с дополнительными материалами:
+                </dt>
+                <dd class="additional-material__value">
+                    <!-- список дополнительных материалов -->
+                    <?php include_once $path."tpl/blocks/additional-materials.php" ?>
+                    <!-- /список дополнительных материалов -->
+                </dd>
+            </dl>
+
+	    </div>
 
 	</div>
 
--- tpl/pages/recuperators/teploizoljatori.php
+++ tpl/pages/recuperators/teploizoljatori.php
@@ -5,10 +5,33 @@
 	<div class="description">
 
 		<div class="description__row">
+
 			<a class="bnrc" href="http://rekuperator.ru/">
 				<img class="bnrc__img" src="/images/pages/recuperators/rekuperator.ru.jpg">
 			</a>
-		</div>
+
+          	<p class="description__text">
+      			Деятельность Бушевецкого завода по производству теплообменников отражена на на сайте "
+      			<a href="http://rekuperator.ru/" title="Пластинчатые рекуператоры БушевецТермо - Производство пластинчатых теплообменников газов. Рекуператоры нового поколения" target="_blank">
+              		Пластинчатые теплообменники
+              	</a>"
+      		</p>
+
+            <p class="description__text"><strong>Дополнительные материалы по теме "Рекуператоры"</strong></p>
+
+            <dl class="additional-material">
+                <dt class="additional-material__label">
+                    Уважаемые посетители сайта, для того, чтобы у вас сложилось представление о разнообразных технологиях и
+                    сферах применения рекуператоров, предлагаем ознакомиться с дополнительными материалами:
+                </dt>
+                <dd class="additional-material__value">
+                    <!-- список дополнительных материалов -->
+                    <?php include_once $path."tpl/blocks/additional-materials.php" ?>
+                    <!-- /список дополнительных материалов -->
+                </dd>
+            </dl>
+
+	    </div>
 
 	</div>
 
