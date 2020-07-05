c4d0ef214dbdb5371ab38e59bd9e2d3bad7c9c9f
bombin78 <bombin78@mail.ru>
2020-07-06 00:24:39 +0300

    Правка верстки

--- tpl/pages/contact.php
+++ tpl/pages/contact.php
@@ -2,20 +2,32 @@
 
 	<h1 class="page__content-title content-title"><?php echo $title; ?></h1>
 
-	<p>
-  <br>
- Site: <a href="http://www.bushevec.ru/">www.bushevec.ru</a> </p>
+	<div class="description">
 
-<p><b>Бушевецкий завод:</b> </p>
+		<div class="description__row">
 
-<p>171076, Россия, Тверская обл., Бологовский р-он, г. Бологое, ул. Народная, д. 9</p>
+			<p class="description__text">
+				Site: <a href="/">www.bushevec.ru</a>
+			</p>
 
-<p> Телефон: +7 (920) 194-84-13</p>
+			<p class="description__text">
+				<b>Бушевецкий завод:</b>
+			</p>
 
-<p> E-mail: <a href="mailto:bushevec@bologoe.tver.ru">bushevec2009@yandex.ru</a>	 </p>
+			<p class="description__text">
+				171076, Россия, Тверская обл., Бологовский р-он, г. Бологое, ул. Народная, д. 9
+			</p>
 
-<p>
-  <br>
- </p>
+			<p class="description__text">
+				Телефон: +7 (920) 194-84-13
+			</p>
+
+			<p class="description__text">
+				E-mail: <a href="mailto:bushevec@bologoe.tver.ru">bushevec2009@yandex.ru</a>
+			</p>
+
+		</div>
+
+	</div>
 
 </section>
--- tpl/pages/error-404.php
+++ tpl/pages/error-404.php
@@ -2,6 +2,12 @@
 
 	<h1 class="page__content-title content-title"><?php echo $title; ?></h1>
 
-	<p class="page__text-error">Запрошенная вами страница не существует</p>
+	<div class="description">
+
+		<div class="description__row">
+			<p class="description__text">Запрошенная вами страница не существует</p>
+		</div>
+
+	</div>
 
 </section>
