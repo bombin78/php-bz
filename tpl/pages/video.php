2c0c674cd8980a7f2cff8226c0e99cd3f8b26512
bombin78 <bombin78@mail.ru>
2020-07-08 00:09:52 +0300

    Страница видео: доработка

--- tpl/pages/video.php
+++ tpl/pages/video.php
@@ -2,24 +2,43 @@
 
 	<h1 class="page__content-title content-title"><?php echo $title; ?></h1>
 
-	<h3>1. Вибропогружение фундамента машиной АВФ-1М.2</h3>
-	<video controls width="400" height="300">
-		<source src="/files/vibropogruzenie.mp4" type="video/mp4">
-	</video>
+	<div class="description">
 
-	<h3>2. Бурение лидирующей скважины оборудованием ТТН-057</h3>
-	<video controls width="400" height="300">
-		<source src="/files/burenie.mp4" type="video/mp4">
-	</video>
+		<div class="description__row">
 
-	<h3>3. Работа устройства по разрушению фундамента ТТН-059</h3>
-	<video controls width="400" height="300">
-		<source src="/files/dolban.mp4" type="video/mp4">
-	</video>
+			<ul class="video">
 
-	<h3>4. Завинчивание винтовых металлических опор ТТН-064</h3>
-	<video controls width="400" height="300">
-		<source src="/files/zavinchivanie.mp4" type="video/mp4">
-	</video>
+				<li class="video__item">
+					<h3 class="video__title">1. Вибропогружение фундамента машиной АВФ-1М.2</h3>
+					<video class="video__data" controls preload="metadata" width="400" height="225">
+						<source src="/files/vibropogruzenie.mp4" type="video/mp4">
+					</video>
+				</li>
+
+				<li class="video__item">
+					<h3 class="video__title">2. Бурение лидирующей скважины оборудованием ТТН-057</h3>
+					<video class="video__data" controls preload="metadata" width="400" height="225">
+						<source src="/files/burenie.mp4" type="video/mp4">
+					</video>
+				</li>
+
+				<li class="video__item">
+					<h3 class="video__title">3. Работа устройства по разрушению фундамента ТТН-059</h3>
+					<video class="video__data" controls preload="metadata" width="400" height="225">
+						<source src="/files/dolban.mp4" type="video/mp4">
+					</video>
+				</li>
+
+				<li class="video__item">
+					<h3 class="video__title">4. Завинчивание винтовых металлических опор ТТН-064</h3>
+					<video class="video__data" controls width="400" height="225">
+						<source src="/files/zavinchivanie.mp4" type="video/mp4">
+					</video>
+				</li>
+
+			</ul>
+		</div>
+
+	</div>
 
 </section>
