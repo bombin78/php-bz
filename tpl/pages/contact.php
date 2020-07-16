0b17534d76c5313ab7c24171a5da8b265aa62d1b
bombin78 <bombin78@mail.ru>
2020-07-16 18:16:48 +0300

    Страница контакты: доработка. Правка стилей

--- tpl/pages/contact.php
+++ tpl/pages/contact.php
@@ -3,29 +3,124 @@
 	<h1 class="page__content-title content-title"><?php echo $title; ?></h1>
 
 	<div class="description">
-
+		
 		<div class="description__row">
 
-			<p class="description__text">
-				Site: <a href="/">www.bushevec.ru</a>
-			</p>
+			<ul class="departments">
 
-			<p class="description__text">
-				<b>Бушевецкий завод:</b>
-			</p>
+				<li class="departments__item">
+					<dl class="department">
+						<dt class="department__label">Машиностроение:</dt>
+						<dd class="department__value">
+							<div class="contacts">
 
+								<ul class="contacts__list">
+
+									<li class="contacts__item">
+										<div class="contacts__post">Начальник производства</div>
+										<div class="contacts__name">Комаров Андрей Юрьевич</div>
+										<div class="contacts__phone">8-920-194-84-19</div>
+									</li>
+
+								</ul>
+
+								<div class="contacts__email">
+									<a href="mailto:a.komarov@brmz69.ru">a.komarov@brmz69.ru</a>
+								</div>
+
+							</div>
+						</dd>
+					</dl>
+				</li>
+
+				<li class="departments__item">
+					<dl class="department">
+						<dt class="department__label">Рекуператоры:</dt>
+						<dd class="department__value">
+							<div class="contacts">
+
+								<ul class="contacts__list">
+
+									<li class="contacts__item">
+										<div class="contacts__post">Менеджер отдела продаж</div>
+										<div class="contacts__phone">8-800-775-93-92</div>
+									</li>
+
+									<li class="contacts__item">
+										<div class="contacts__post">Главный инженер</div>
+										<div class="contacts__name">Калягин Сергей Николаевич</div>
+										<div class="contacts__phone">8-920-193-75-00</div>
+									</li>
+
+								</ul>
+
+								<div class="contacts__email">
+									<a href="mailto:bushevec5@yandex.ru">bushevec5@yandex.ru</a>
+								</div>
+
+							</div>
+						</dd>
+					</dl>
+				</li>
+
+				<li class="departments__item">
+					<dl class="department">
+						<dt class="department__label">Ремонт ССПС (специального самоходного подвижного состава):</dt>
+						<dd class="department__value">
+							<div class="contacts">
+
+								<ul class="contacts__list">
+
+									<li class="contacts__item">
+										<div class="contacts__post">Начальник ремонтного участка</div>
+										<div class="contacts__name">Андреев Александр Анатольеви</div>
+										<div class="contacts__phone">8-920-194-84-11</div>
+									</li>
+
+								</ul>
+
+								<div class="contacts__email">
+									<a href="mailto:a.andreev@brmz69.ru">a.andreev@brmz69.ru</a>
+								</div>
+
+							</div>
+						</dd>
+					</dl>
+				</li>
+
+				<li class="departments__item">
+					<dl class="department">
+						<dt class="department__label">Отдел снабжения и сбыта:</dt>
+						<dd class="department__value">
+							<div class="contacts">
+
+								<ul class="contacts__list">
+
+									<li class="contacts__item">
+										<div class="contacts__post">Исполняющий обязанности начальника ОМТС</div>
+										<div class="contacts__name">Семиткина Наталья Анатольевна</div>
+										<div class="contacts__phone">8-920-194-84-22</div>
+									</li>
+
+								</ul>
+
+								<div class="contacts__email">
+									<a href="mailto:n.semitkina@brmz69.ru">n.semitkina@brmz69.ru</a>
+								</div>
+
+							</div>
+						</dd>
+					</dl>
+				</li>
+
+			</ul>
+
+		</div>
+
+		<div class="description__bottom">
 			<p class="description__text">
 				171076, Россия, Тверская обл., Бологовский р-он, г. Бологое, ул. Народная, д. 9
 			</p>
-
-			<p class="description__text">
-				Телефон: +7 (920) 194-84-13
-			</p>
-
-			<p class="description__text">
-				E-mail: <a href="mailto:bushevec@bologoe.tver.ru">bushevec2009@yandex.ru</a>
-			</p>
-
 		</div>
 
 	</div>
