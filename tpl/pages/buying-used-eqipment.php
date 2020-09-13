f6f1edb6b3962873ed5ba8ff76a191ff3c7b24c3
bombin78 <bombin78@mail.ru>
2020-09-13 13:10:36 +0300

    Раздел "Приобретение б/у техники": правка. Раздел "Материалы": убран подраздел "Мотовоз, ремонт мотовоза"

---
+++ tpl/pages/buying-used-eqipment.php
@@ -0,0 +1,19 @@
+<section class="page">
+
+	<h1 class="page__content-title content-title"><?php echo $title; ?></h1>
+
+	<div class="description">
+
+		<div class="description__row">
+
+			<ul class="list">
+				<li class="list__item">Фитинговые платформы модели 2116</li>
+				<li class="list__item">Дрезины АДМ, МПТ</li>
+				<li class="list__item">Вибропогружатели: машины АВФ и МС</a></li>
+			</ul>
+
+		</div>
+
+	</div>
+
+</section>
--- tpl/pages/buying-used-eqipment/fitting-platforms.php
+++
@@ -1,5 +0,0 @@
-<section class="page">
-
-	<h1 class="page__content-title content-title"><?php echo $title; ?></h1>
-
-</section>
