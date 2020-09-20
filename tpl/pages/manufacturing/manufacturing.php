e3c39a5467b2ffe46fd847d21e228ded9f664af8
bombin78 <bombin78@mail.ru>
2020-09-20 20:19:45 +0300

    Страница "Изготовление новых машин": правка. Убраны лишние страницы + права файла .htaccess

--- tpl/pages/manufacturing/manufacturing.php
+++ tpl/pages/manufacturing/manufacturing.php
@@ -6,29 +6,181 @@
 
 		<div class="bz-description__row">
 
-			<dl class="bz-detail">
-				<dt class="bz-detail__label">
-					Разделы:
-				</dt>
-				<dd class="bz-detail__value">
+			<h2 class="bz-description__title">Агрегат для вибропогружения свайных фундаментов – АВФ-1М.2</h2>
 
-					<ul class="bz-list">
-						<li class="bz-list__item">
-							<a class="bz-list__link" href="/<?= $routesParams[5]['items'][0][$linkParam]?>">
-								Котлованокопатели
-							</a>
-						</li>
-						<li class="bz-list__item">
-							<a class="bz-list__link" href="/<?= $routesParams[5]['items'][1][$linkParam]?>">
-								Вибропогружатели – АВФ-1
-							</a>
-						</li>
-					</ul>
+			<p class="bz-description__text">
+				ООО «Бушевецкий завод» изготавливает и поставляет агрегат для вибропогружения свайных фундаментов АВФ-1М.2
+				и его модификации для подразделений ОАО «РЖД» занятых на электрификации железных дорог, текущем и
+				капитальном ремонте контактной сети, и сторонних организаций занятых аналогичными видами работ на контактной
+				сети.
+			</p>
 
-				</dd>
-			</dl>
+			<div class="bz-img-wrap">
+				<img
+					class="bz-img"
+					alt="вибропогружатель АВФ-1, Бушевецкий завод "
+					src="/images/pages/manufacturing/AVF-1.jpg"
+					vspace="5"
+					width="600"
+					height="450"
+					hspace="5">
+			</div>
+
+			<h3 class="bz-description__subtitle">Назначение:</h2>
+
+			<p class="bz-description__text">
+				Агрегат предназначен для вибропогружения с железнодорожного пути свайных фундаментов типа ТС, ТСН, ТА,
+				ФК, ФКА, ТФПЭ и ТФПА опор контактной сети и железобетонных свай 35х35 см, а также для сооружения сборного
+				фундамента опор гибких поперечин при электрификации и реконструкции железных дорог нормальной колеи,
+				текущем и капитальном ремонте контактной сети железных дорог.
+			</p>
+
+			<p class="bz-description__text">
+				Агрегат приспособлен для работы в климатических зонах с умеренным климатом при температуре окружающего
+				воздуха от минус 25 0С до плюс 40 0С в талых грунтах I и II групп по классификации СНиП IV-5-82
+				(песчаные глинистые грунты средней плотности различной крупности для песка и консистенции для глины).
+			</p>
 
 		</div>
+
+		<div class="bz-description__row">
+
+			<div class="bz-img-wrap">
+				<img
+					class="bz-img"
+					alt="вибропогружатель АВФ-1, схема"
+					src="/images/pages/manufacturing/vibro-avf-1.gif"
+					vspace="5"
+					width="600"
+					align="absmiddle"
+					height="190"
+					hspace="5">
+			</div>
+
+			<p class="bz-description__text">
+				Дизель-электрический агрегат АД-100С-Т400-РМ2; 2. Элекрооборудование; 3. Кабина; 4. Рама направляющая;
+				5. Ферма выдвижная; 6. Ферма поворотная; 7. Вибропогружатель; 8. Железнодорожная платформа Модель 13—2116;
+				9. Кран манипулятор ИНМАН-Палфингер ИМ-320.
+			</p>
+
+		</div>
+
+		<div class="bz-description__row">
+
+			<h3 class="bz-description__subtitle">Технические характеристики:</h2>
+
+			<table class="bz-params">
+
+			    <tbody class="bz-params__body">
+
+			        <tr class="bz-params__body-row">
+			            <td class="bz-params__body-cell">
+							Техническая производительность вибропогружения свайных фундаментов, шт/ч, не более
+						</td>
+			            <td class="bz-params__body-cell">3</td>
+			        </tr>
+
+			        <tr class="bz-params__body-row">
+			            <td class="bz-params__body-cell">
+							Масса погружаемого фундамента или сваи наибольшая, т 
+						</td>
+			            <td class="bz-params__body-cell">2,7</td>
+			        </tr>
+
+			        <tr class="bz-params__body-row">
+			            <td class="bz-params__body-cell">
+							Длина погружаемого фундамента наибольшая, м 
+						</td>
+			            <td class="bz-params__body-cell">5,0</td>
+			        </tr>
+
+					<tr class="bz-params__body-row">
+						<td class="bz-params__body-cell">
+							Длина погружаемой сваи 35х35 см наибольшая, м
+						</td>
+						<td class="bz-params__body-cell">8,0</td>
+					</tr>
+
+			        <tr class="bz-params__body-row">
+			            <td class="bz-params__body-cell">
+							Глубина погружения фундамента от уровня головки рельса наибольшая, м 
+						</td>
+			            <td class="bz-params__body-cell">6,15</td>
+			        </tr>
+
+			        <tr class="bz-params__body-row">
+			            <td class="bz-params__body-cell">
+							Пределы расстояний от оси ж. д. пути до оси  погружаемого фундамента, м 
+						</td>
+			            <td class="bz-params__body-cell">3,35..5,95<sub>*</sub></td>
+			        </tr>
+
+			        <tr class="bz-params__body-row">
+			            <td class="bz-params__body-cell" colspan="2">
+							Скорость передвижения агрегата самоходом в сцепе <nobr>с 4-х осной</nobr>
+							платформой с грузом массой до 20 т, км/ч
+						</td>
+			        </tr>
+
+			        <tr class="bz-params__body-row">
+			            <td class="bz-params__body-item">
+							а) на горизонтальном прямом участке
+						 </td>
+			            <td class="bz-params__body-cell">14</td>
+			        </tr>
+
+			        <tr class="bz-params__body-row">
+			            <td class="bz-params__body-item">
+							б) в кривых радиусом 150 м
+						 </td>
+			            <td class="bz-params__body-cell">6</td>
+			        </tr>
+
+			        <tr class="bz-params__body-row">
+			            <td class="bz-params__body-item">
+							в) на подъеме до 10%
+						</td>
+			            <td class="bz-params__body-cell">6</td>
+			        </tr>
+
+			        <tr class="bz-params__body-row">
+			            <td class="bz-params__body-cell">
+							Наибольшая допустимая скорость передвижения агрегата в составе грузового поезда
+							на железных дорогах общей сети РФ, км/ч
+						</td>
+			            <td class="bz-params__body-cell">70</td>
+			        </tr>
+
+			        <tr class="bz-params__body-row">
+			            <td class="bz-params__body-cell">
+							Масса агрегата в снаряженном состоянии без загрузки его фундаментами, т
+						</td>
+			            <td class="bz-params__body-cell">58</td>
+			        </tr>
+
+			        <tr class="bz-params__body-row">
+			            <td class="bz-params__body-cell">
+							Масса агрегата в снаряженном состоянии без загрузки его фундаментами, не более, т 
+						</td>
+			            <td class="bz-params__body-cell">80</td>
+			        </tr>
+
+			        <tr class="bz-params__body-row">
+			            <td class="bz-params__body-cell">Габарит </td>
+			            <td class="bz-params__body-cell">Т</td>
+			        </tr>
+
+			    </tbody>
+
+				<tfoot class="bz-params__foot">
+					<tr class="bz-params__foot-row">
+						<td class="bz-params__foot-cell" colspan="2">
+							* Возможно исполнение с минимальным расстоянием от оси ж. д. пути до оси  погружаемого фундамента – 2,75 м.
+						</td>
+					</tr>
+				</tfoot>
+
+			</table>
 
 	</div>
 
