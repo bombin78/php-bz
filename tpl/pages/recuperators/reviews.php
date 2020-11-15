79f3afbefe8afe1bd6219f6986741915ca250baf
bombin78 <bombin78@mail.ru>
2020-11-16 00:11:46 +0300

    Раздел "Рекуператор тепла": Добавлена страница "Отзывы"

---
+++ tpl/pages/recuperators/reviews.php
@@ -0,0 +1,54 @@
+<section class="bz-page">
+
+	<h1 class="bz-page__content-title bz-content-title"><?php echo $title; ?></h1>
+
+	<div class="bz-description">
+
+		<ul class="bz-slider blueimp-links">
+
+			<li class="bz-slider__item">
+				<a class="bz-slider__link" href="/images/pages/recuperators/reviews/big/reviewabsolut.jpg" title="">
+					<img class="bz-slider__img" src="/images/pages/recuperators/reviews/reviewabsolut.jpg" alt="">
+				</a>
+			</li>
+
+			<li class="bz-slider__item">
+				<a class="bz-slider__link" href="/images/pages/recuperators/reviews/big/reviewlaz.jpg" title="">
+					<img class="bz-slider__img" src="/images/pages/recuperators/reviews/reviewlaz.jpg" alt="">
+				</a>
+			</li>
+
+			<li class="bz-slider__item">
+				<a class="bz-slider__link" href="/images/pages/recuperators/reviews/big/reviewlider.jpg" title="">
+					<img class="bz-slider__img" src="/images/pages/recuperators/reviews/reviewlider.jpg" alt="">
+				</a>
+			</li>
+
+			<li class="bz-slider__item">
+				<a class="bz-slider__link" href="/images/pages/recuperators/reviews/big/reviewnevz.jpg" title="">
+					<img class="bz-slider__img" src="/images/pages/recuperators/reviews/reviewnevz.jpg" alt="">
+				</a>
+			</li>
+
+			<li class="bz-slider__item">
+				<a class="bz-slider__link" href="/images/pages/recuperators/reviews/big/reviewsolid.jpg" title="">
+					<img class="bz-slider__img" src="/images/pages/recuperators/reviews/reviewsolid.jpg" alt="">
+				</a>
+			</li>
+
+			<li class="bz-slider__item">
+				<a class="bz-slider__link" href="/images/pages/recuperators/reviews/big/reviewvpk.jpg" title="">
+					<img class="bz-slider__img" src="/images/pages/recuperators/reviews/reviewvpk.jpg" alt="">
+				</a>
+			</li>
+
+			<li class="bz-slider__item">
+				<a class="bz-slider__link" href="/images/pages/recuperators/reviews/big/reviewzsk.jpg" title="">
+					<img class="bz-slider__img" src="/images/pages/recuperators/reviews/reviewzsk.jpg" alt="">
+				</a>
+			</li>
+
+		</ul>
+	</div>
+
+</section>
